76069a03713d2d89a283cc37af529c21607090e4
Mohamad Olleik <mao21@mail.aub.edu>
2018-02-17 16:23:54 +0200

    FireBase connected to sign in and sign up pages

--- Survay_1/app/src/main/java/com/example/user/survay_1/MainActivity.java
+++ Survay_1/app/src/main/java/com/example/user/survay_1/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.user.survay_1;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +23,45 @@ public class MainActivity extends AppCompatActivity {
     private EditText Password;
     private Button Login;
     private TextView goToRegister;
+    private FirebaseAuth mAuth;
 
+    private void SignInUser(){
+        String emailString = Email.getText().toString().trim();
+        String passwordString = Password.getText().toString().trim();
+        if(emailString.isEmpty()){
+            Email.setError("Please enter an email to sign up");
+            Email.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(emailString).matches()){
+            Email.setError("Please enter a valid email to sign in");
+            Email.requestFocus();
+            return;
+        }
+        if(passwordString.isEmpty()){
+            Password.setError("Please enter a password to sign in");
+            Password.requestFocus();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(emailString, passwordString)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(),"Sign In successful",Toast.LENGTH_SHORT).show();
+                            Intent myIntent = new Intent(MainActivity.this, SecondActivity.class);
+                            MainActivity.this.startActivity(myIntent);
+                        } else {
+                            // If sign in fails, display a message to the user
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
 
 
     @Override
@@ -23,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
         Email = (EditText) findViewById(R.id.email_editText);
         Password = (EditText) findViewById(R.id.password_editText);
         Login = (Button) findViewById(R.id.login_Button);
@@ -40,9 +87,8 @@ public class MainActivity extends AppCompatActivity {
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
                 public void onClick(View v) {
-              Intent myIntent = new Intent(MainActivity.this, MainActivity.class);
-                MainActivity.this.startActivity(myIntent);
-                validate(Email.getText().toString(), Password.getText().toString());
+           SignInUser();
+
             }
         });
 
@@ -50,11 +96,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void validate(String userName, String userPassword) {
-        if (userName.equals("123") && userPassword.equals("123")) {
-            Intent myIntent = new Intent(MainActivity.this, SecondActivity.class);
-            MainActivity.this.startActivity(myIntent);
-        }
-    }
+
 }
 
--- Survay_1/app/src/main/java/com/example/user/survay_1/RegisterActivity.java
+++ Survay_1/app/src/main/java/com/example/user/survay_1/RegisterActivity.java
@@ -1,21 +1,68 @@
 package com.example.user.survay_1;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.*;
+
 
 public class RegisterActivity extends AppCompatActivity {
-    private EditText Name;
-    private EditText Email;
-    private EditText Password;
-    private Button Register;
-    private TextView goToLogin;
+    private EditText Name ;
+    private EditText Email ;
+    private EditText Password ;
+    private Button Register ;
+    private TextView goToLogin ;
+    private FirebaseAuth mAuth;
 
-    @Override
+
+    private void RegisterUser(){
+        String nameString = Name.getText().toString().trim();
+        String emailString = Email.getText().toString().trim();
+        String passwordString = Password.getText().toString().trim();
+        if(nameString.isEmpty()){
+            Name.setError("Please enter your full name to sign up");
+            Name.requestFocus();
+            return;
+        }
+        if(emailString.isEmpty()){
+                Email.setError("Please enter an email to sign up");
+                Email.requestFocus();
+                return;
+            }
+        if(!Patterns.EMAIL_ADDRESS.matcher(emailString).matches()){
+                Email.setError("Please enter a valid email to sign up");
+                Email.requestFocus();
+                return;
+        }
+        if(passwordString.isEmpty()){
+                    Password.setError("Please enter a password to sign up");
+                    Password.requestFocus();
+                    return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(),"Sign up successful",Toast.LENGTH_SHORT).show();
+                    Intent myIntent = new Intent(RegisterActivity.this, SecondActivity.class);
+                    RegisterActivity.this.startActivity(myIntent);
+                }
+        }
+    });
+    }
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
@@ -24,7 +71,7 @@ public class RegisterActivity extends AppCompatActivity {
         Password = (EditText) findViewById(R.id.password_editText);
         Register = (Button) findViewById(R.id.register_Button);
         goToLogin = (TextView) findViewById(R.id.goToLogin_TextView);
-
+        mAuth = FirebaseAuth.getInstance();
         //Go To Login text will send you to the login activity when clicked on
         goToLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,17 +80,19 @@ public class RegisterActivity extends AppCompatActivity {
                 RegisterActivity.this.startActivity(myIntent);
             }
         });
-        {
+
 
             Register.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent myIntent = new Intent(RegisterActivity.this, SecondActivity.class);
-                    RegisterActivity.this.startActivity(myIntent);
+                    RegisterUser();
+
                 }
             });
 
 
         }
+
     }
-}
+
+
